5124d495f2d9582cd3d147a3cc1d1d9c959ac06d
asyndesis <trevorbonomi@gmail.com>
2024-06-13 20:11:41 +0200

    redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse, type NextRequest } from "next/server";
 
 const isOnboardingRoute = createRouteMatcher(["/onboarding"]);
+const isRootRoute = createRouteMatcher(["/"]);
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
@@ -11,18 +12,19 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware((auth, req: NextRequest) => {
   const { userId, sessionClaims, redirectToSignIn } = auth();
+  const doneOnboarding = sessionClaims?.metadata?.onboardingComplete;
 
-  // If the user isn't signed in and the route is private, redirect to sign-in
-  if (!userId && !isPublicRoute(req))
+  if (!userId && !isPublicRoute(req)) {
     return redirectToSignIn({ returnBackUrl: req.url });
+  }
 
-  // If the user is signed in
   if (userId) {
-    // If user has not completed onboarding, and is on any route other than onboarding route
-    // redirect them to onboarding. Completed onboarding means they've been added to our database.
-    const doneOnboarding = sessionClaims?.metadata?.onboardingComplete;
     if (!doneOnboarding && !isOnboardingRoute(req)) {
       return NextResponse.redirect(new URL("/onboarding", req.url));
+    }
+
+    if (isRootRoute(req)) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
   }
 
